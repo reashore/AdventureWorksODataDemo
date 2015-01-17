657ecdf32ab560c8e7dcb8ec7504555140098286
Frank J. Reashore <reashore@telus.net>
2015-01-17 10:41:21 -0800

    Initial commit

---
+++ AdventureWorksODataDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+using System.Linq;
+using AdventureWorksODataDemo.AdventureWorksService;
+
+namespace AdventureWorksODataDemo
+{
+	class Program
+	{
+		static void Main()
+		{
+			Uri adventureWorksServiceUri = new Uri("http://services.odata.org/AdventureWorksV3/AdventureWorks.svc/");
+			AdventureWorksEntities adventureWorksEntities = new AdventureWorksEntities(adventureWorksServiceUri);
+
+			var query = from p in adventureWorksEntities.ProductCatalog select p;
+
+			foreach (vProductCatalog p in query)
+			{
+				Console.WriteLine("{0} {1}",p.ID, p.ProductName);
+			}
+
+			Console.ReadKey();
+		}
+	}
+}
